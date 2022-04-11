86faf5432885e0709e4c593262308353eaf473f8
BabichAndrey <andrei.babich@azati.com>
2022-04-11 12:02:51 +0300

    Init commit

---
+++ src/test/java/com/grsu/map/InteractiveMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grsu.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
