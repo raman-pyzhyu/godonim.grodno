d821e6e4a232c1f9cbbc2a7cdacebd92d41a3dda
BabichAndrey <andrei.babich@azati.com>
2022-04-27 00:09:20 +0300

    Code refactored

--- src/main/java/com/grsu/map/service/MediaService.java
+++ src/main/java/com/grsu/map/service/MediaService.java
@@ -33,8 +33,8 @@ public class MediaService {
 
         try {
             file.transferTo(new File(uploadPath + "/" + file.getOriginalFilename()));
-        } catch (IOException e) {
-
+        } catch (IOException ignored) {
+            //TODO
         }
 
         return file.getOriginalFilename();
@@ -50,7 +50,7 @@ public class MediaService {
 
     public void deleteMedia(long id) {
         Media media = mediaRepository.getById(id);
-        if (new File(uploadPath + "/" +media.getFileName()).delete()) {
+        if (new File(uploadPath + "/" + media.getFileName()).delete()) {
             mediaRepository.deleteById(id);
         }
     }
